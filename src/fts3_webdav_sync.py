114eef97d96cdbd8d1515ff2ef86962171372dd5
Tobias Wochinger <tobias.wochinger@student.htw-berlin.de>
2017-01-14 18:48:00 +0100

    closes #8 Rerun after time interval

--- src/fts3_webdav_sync.py
+++ src/fts3_webdav_sync.py
@@ -4,31 +4,43 @@ import file_tree.directory as tree
 from time import time, sleep
 import argparse
 from configuration.configuration import read_configuration_file
+import logging
 
 
 def start_synchronizing(configuration_file_path):
-    configuration = read_configuration_file(configuration_file_path)
-    source_client = wc.Client(configuration.dav.source_options)
-    source_tree = tree.Directory(source_client, 'webdav/')
-    source_tree.populate()
+    while True:
+        start_time = time()
+        configuration = read_configuration_file(configuration_file_path)
+        logging.info('Start Synchronization')
+        logging.debug('Getting the contents')
+        source_client = wc.Client(configuration.dav.source_options)
+        source_tree = tree.Directory(source_client, 'webdav/')
+        source_tree.populate()
 
-    destination_client = wc.Client(configuration.dav.destination_options)
-    destination_tree = tree.Directory(destination_client, 'webdav/')
-    destination_tree.populate()
+        destination_client = wc.Client(configuration.dav.destination_options)
+        destination_tree = tree.Directory(destination_client, 'webdav/')
+        destination_tree.populate()
 
-    file_diff = source_tree.diff_tree(destination_tree)
-    print('NEW')
-    print(file_diff.new_files())
+        logging.debug('Comparing the contents')
+        file_diff = source_tree.diff_tree(destination_tree)
 
-    print('Modified')
-    print(file_diff.modified_files())
+        logging.info('New files:\n{}'.format(file_diff.new_files()))
+        logging.info('Modified files:\n{}'.format(file_diff.modified_files()))
 
-    print('Removed')
-    print(file_diff.removed_files())
+        if not configuration.sync_settings.dry_run:
+            fts = FTS(configuration)
+            fts.submit(file_diff)
+        else:
+            logging.debug('Not submitting changes as we are running in dry mode')
 
-    if not configuration.dry_run:
-        fts = FTS(configuration)
-        fts.submit(file_diff)
+        if configuration.sync_settings.single_run:
+            break
+        else:
+            sync_duration = time() - start_time
+            time_till_next_run = configuration.sync_settings.interval - sync_duration
+            logging.info('Synchronization took {} seconds'.format(sync_duration))
+            logging.debug('Next run will start in {} minutes'.format(time_till_next_run / 60.0))
+            sleep(time_till_next_run)
 
 
 def main():
