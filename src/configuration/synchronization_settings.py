bc2d48bb494e75c2764975410adba52c0e499ed7
Tobias Wochinger <tobias.wochinger@student.htw-berlin.de>
2017-01-14 18:47:11 +0100

    added option for single run

--- src/configuration/synchronization_settings.py
+++ src/configuration/synchronization_settings.py
@@ -9,3 +9,4 @@ class SynchronizationSetting(object):
         self.strategy = config.get('action for modified file', IGNORE_MODIFIED)        self.interval = config.get('sync interval in minutes', 30) * 60
         self.interval = config.get('sync interval in minutes', 30) * 60
         self.dry_run = config.get('dry run', False)
+        self.single_run = config.get('single run', False)
