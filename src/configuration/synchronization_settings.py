179b133b8bf63eca9be1ed4c02fb35527018f265
Tobias Wochinger <tobias.wochinger@student.htw-berlin.de>
2017-01-14 18:48:59 +0100

    removed duplicate setting

--- src/configuration/synchronization_settings.py
+++ src/configuration/synchronization_settings.py
@@ -6,7 +6,7 @@ KEEP_BOTH = 'keep_both'
 class SynchronizationSetting(object):
 
     def __init__(self, config):
-        self.strategy = config.get('action for modified file', IGNORE_MODIFIED)        self.interval = config.get('sync interval in minutes', 30) * 60
+        self.strategy = config.get('action for modified file', IGNORE_MODIFIED)
         self.interval = config.get('sync interval in minutes', 30) * 60
         self.dry_run = config.get('dry run', False)
         self.single_run = config.get('single run', False)
