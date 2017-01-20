2d677ac4f0361c524475f0b465942a2474712fa6
Tobias Wochinger <tobias.wochinger@student.htw-berlin.de>
2017-01-20 11:45:18 +0100

    fixed detection of zero changes

--- fts_sync/fts/fts.py
+++ fts_sync/fts/fts.py
@@ -17,14 +17,18 @@ class FTS(object):
         self.strategy = config.sync_settings.strategy
 
     def submit(self, file_differences):
-        if len(file_differences) < 1 or (len(file_differences.new_files()) < 1 and self.strategy == SyncStrategy.IGNORE_MODIFIED):
+
+        new_files = file_differences.new_files()
+        modified_files = file_differences.modified_files()
+        total_changes = len(new_files) + (0 if SyncStrategy.IGNORE_MODIFIED else len(modified_files))
+        if total_changes < 1:
             logger.info('No changes to synchronize')
             return
 
-        transfers_for_new_file = map(self._create_transfer, file_differences.new_files())
+        transfers_for_new_file = map(self._create_transfer, new_files)
         transfers_for_modified = []
         if self.strategy != SyncStrategy.IGNORE_MODIFIED and len(file_differences.modified_files()) > 0:
-            transfers_for_modified = map(self._create_transfers_for_modified, file_differences.modified_files())
+            transfers_for_modified = map(self._create_transfers_for_modified, modified_files)
 
         all_transfers = transfers_for_new_file + transfers_for_modified
         job = fts3.new_job(all_transfers, verify_checksum=False, overwrite=self.strategy == SyncStrategy.OVERWRITE_MODIFIED)
