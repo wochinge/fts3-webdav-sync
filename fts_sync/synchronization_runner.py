e1504c2c59cb874232ff603c3eb65f9a03866c96
Tobias Wochinger <tobias.wochinger@student.htw-berlin.de>
2017-04-18 08:24:09 +0200

    refactoring and properly joining of the processes

--- fts_sync/synchronization_runner.py
+++ fts_sync/synchronization_runner.py
@@ -20,15 +20,16 @@ class SynchronizationRunner(object):
     def run(self):
         while True:
             logger.info('Start Synchronization')
-            start_time = time()
+            start_time_of_synchronization = time()
 
             self.configuration = self.__get_configuration()
-            next = time()
+            start_time_of_current_step = time()
             source_tree, destination_tree = self.__index_dav_servers()
-            print(time() - next)
-            next = time()
+            logger.debug('It took {} seconds to index the servers'.format(time() - start_time_of_current_step))
+
+            start_time_of_current_step = time()
             file_diff = self.__calculate_delta(source_tree, destination_tree)
-            print(time() - next)
+            logger.debug('It tooks {} seconds to calculate the differences'.format(time() - start_time_of_current_step))
 
             if not self.configuration.sync_settings.dry_run:
                 fts = FTS(self.configuration)
@@ -36,32 +37,37 @@ class SynchronizationRunner(object):
             else:
                 logger.debug('Not submitting changes as we are running in dry mode')
 
-            logger.info('Synchronization took {} seconds'.format(time() - start_time))
+            logger.info('Synchronization took {} seconds'.format(time() - start_time_of_synchronization))
 
             if self.configuration.sync_settings.single_run:
                 break
             else:
-                self.__sleep_till_next_run(start_time)
+                self.__sleep_till_next_run(start_time_of_synchronization)
 
     def __get_configuration(self):
-        logger.info('Read configuration')
+        logger.debug('Read configuration')
         return read_configuration_file(self.configuration_file_path)
 
     def __index_dav_servers(self):
         logger.debug('Getting the contents')
-        source_tree_queue = self.__populate_file_tree(self.configuration.source_url,
-                                                      self.configuration.dav.source_start_directory)
-        destination_tree_queue = self.__populate_file_tree(self.configuration.destination_url,
-                                                           self.configuration.dav.destination_start_directory)
+        source_tree_queue, source_tree_process = self.__populate_file_tree(self.configuration.source_url,
+                                                                           self.configuration.dav.source_start_directory)
+        destination_tree_queue, destination_tree_process = self.__populate_file_tree(self.configuration.destination_url,
+                                                                                     self.configuration.dav.destination_start_directory)
 
-        return source_tree_queue.get(), destination_tree_queue.get()
+        source_file_tree = source_tree_queue.get()
+        destination_tree = destination_tree_queue.get()
+        source_tree_process.join()
+        destination_tree_process.join()
+
+        return source_file_tree, destination_tree
 
     def __populate_file_tree(self, host, start_directory=''):
         client = DavClient(host, self.configuration.dav, self.configuration.sync_settings.excluded)
         queue = Queue()
         process = Process(target=client.list, args=(queue, start_directory))
         process.start()
-        return queue
+        return queue, process
 
     def __calculate_delta(self, source_tree, destination_tree):
         logger.debug('Comparing the contents')
